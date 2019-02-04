1d8642754e285182bd836b72df0c9c8877ba07b9
Jean-Marie Couteyen <jean-marie.couteyen-carpaye@airbus.com>
2019-02-04 11:01:27 +0100

    package struct

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcvb",
+    version="0.0.1",
+    author="Airbus SAS",
+    author_email="jean-marie.couteyen-carpaye@airbus.com",
+    description="A package ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
