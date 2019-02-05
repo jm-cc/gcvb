4be8771c3ff468baad20c1e4544c225d0f751366
Jean-Marie Couteyen <jean-marie.couteyen-carpaye@airbus.com>
2019-02-05 15:16:06 +0100

    filters now works

--- gcvb/command_line.py
+++ gcvb/command_line.py
@@ -1,5 +1,6 @@
 import argparse
 import yaml
+import re
 from . import yaml_input
 
 def parse():
@@ -7,11 +8,11 @@ def parse():
 
     #filters options
     parser.add_argument('--yaml-file',metavar="filename",default="test.yaml")
-    parser.add_argument('--filter-by-pack',metavar="pack_id",help="comma-separated list of packs to filter",nargs=1,type=str)
+    parser.add_argument('--filter-by-pack',metavar="regexp",help="Regexp to select packs")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('--filter-by-tag',metavar="tag",help="a tag name to filter tests",nargs=1,type=str)
-    group.add_argument('--filter-by-tag-and',metavar="tag_list",help="comma-separated list of tags to filter tests (AND operator)",nargs=1,type=str)
-    group.add_argument('--filter-by-tag-or',metavar="tag_list",help="comma-separated list of tags to filter tests (OR operator)",nargs=1,type=str)
+    group.add_argument('--filter-by-tag',metavar="tag",help="a tag name to filter tests")
+    group.add_argument('--filter-by-tag-and',metavar="tag_list",help="comma-separated list of tags to filter tests (AND operator)")
+    group.add_argument('--filter-by-tag-or',metavar="tag_list",help="comma-separated list of tags to filter tests (OR operator)")
     
     subparsers = parser.add_subparsers(dest="command")
     parser_generate = subparsers.add_parser('generate', help="generate a new gcvb instance")
@@ -23,6 +24,21 @@ def parse():
 def main():
     args=parse()
     a=yaml_input.load_yaml(args.yaml_file)
+    # Filters
+    if (args.filter_by_pack):
+        a["Packs"]=[p for p in a["Packs"] if re.match(args.filter_by_pack,p["pack_id"])]
+    if (args.filter_by_tag):
+        for e in a["Packs"]:
+            e["Tests"]=[t for t in e["Tests"] if args.filter_by_tag in t.get("tags",[])]
+    if (args.filter_by_tag_and):
+        tags=set(args.filter_by_tag_and.split(","))
+        for e in a["Packs"]:
+            e["Tests"]=[t for t in e["Tests"] if (tags.intersection(set(t.get("tags",[])))==tags)]
+    if (args.filter_by_tag_or):
+        tags=set(args.filter_by_tag_or.split(","))
+        for e in a["Packs"]:
+            e["Tests"]=[t for t in e["Tests"] if (tags.intersection(set(t.get("tags",[])))!=set())]
+    #Commands
     if args.command=="list":
         print(yaml.dump(a))
 
