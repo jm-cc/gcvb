2469c9f89a215898c298c9c9cbc35ace6709f4c3
Jean-Marie Couteyen <jean-marie.couteyen-carpaye@airbus.com>
2019-03-19 17:56:06 +0100

    user_lib functions (get_tests,get_file_list,retrieve_file)

--- gcvb/user_lib.py
+++ gcvb/user_lib.py
@@ -9,3 +9,18 @@ def add_metric(name, value):
     test_id=os.environ["GCVB_TEST_ID"] # string as in the yaml file.
     run_id=os.environ["GCVB_RUN_ID"] # integer id
     db.add_metric(run_id, test_id, name, value)
+
+def get_tests(run=None):
+    if not(run):
+        run=db.get_last_run()[0]
+    return [row["name"] for row in db.get_tests(run)]
+
+def get_file_list(test, run=None):
+    if not(run):
+        run=db.get_last_run()[0]
+    return db.get_file_list(run, test)
+
+def retrieve_file(test, filename, run=None):
+    if not(run):
+        run=db.get_last_run()[0]
+    return db.retrieve_file(run, test, filename)
