51889704dc7c23cd67d086f1f522fd36e3f11a01
Jean-Marie Couteyen <jean-marie.couteyen-carpaye@airbus.com>
2019-03-25 12:42:46 +0100

    description of the package + remove ruamel as a dependency - only needed for conversion script that are not really part of gcvb

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Airbus SAS",
     author_email="jean-marie.couteyen-carpaye@airbus.com",
-    description="A package ",
+    description="A package which goal is to simplify non-regression testing for simulation code",
     long_description=long_description,
     long_description_content_type="text/markdown",
     #url="https://github.com/pypa/sampleproject",
@@ -18,4 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=["PyYAML"]
 )
