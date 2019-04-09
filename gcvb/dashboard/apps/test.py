0b55c5e515aabf492e7011e2906ced4b1d8ba21f
Jean-Marie Couteyen <jean-marie.couteyen-carpaye@airbus.com>
2019-04-09 18:55:52 +0200

    temporary layout for tests - fake data

--- gcvb/dashboard/apps/test.py
+++ gcvb/dashboard/apps/test.py
@@ -18,31 +18,60 @@ def data_preparation(run, test_id):
     return str(base["Tests"][test_id])
 
 #Content
+
+def metric_table(list_of_metrics):
+    header=html.Tr([html.Th(col) for col in ["Metric","Type","Distance","Target"]])
+
+    rows = []
+    for m in list_of_metrics:
+        row = []
+        for col in ["id", "type", "distance", "tolerance"]:
+            cell = html.Td(m[col])
+            row.append(cell)
+
+        style=""
+        if (float(m["distance"])>float(m["tolerance"])):
+            style="table-danger"
+
+        rows.append(html.Tr(row,className=style))
+    return html.Table(html.Tbody([header]+rows),className="table table-sm table-bordered")
+
 def summary_panel(data):
-    #Lines of the table
-    description_line=html.Tr([html.Th("Description"),html.Td(data["description"])])
+    status_str=html.Span("Success",className="badge badge-success")
+    if data["status"]!="success":
+        status_str=html.Span("Failure",className="badge badge-danger")
+
+    title=html.H1([data["test_id"]+" ",status_str])
+    description_block=html.Div([html.H5("Description"),html.P(data["description"])],id="description")
+
     result_line=html.Tr([html.Th("Result"),(html.Td(data["result"]))])
     time_line=html.Tr([html.Th("Elapsed time (s)"),(html.Td(data["time"]))])
 
     #Table
-    table=html.Table(html.Tbody([description_line,result_line,time_line]), className="table table-sm")
+    table=html.Table(html.Tbody([result_line,time_line]), className="table table-sm")
 
     #Panel
     panel_header=html.Div(html.H3("Summary",className="panel-title"),className="panel panel-heading")
     panel=html.Div([panel_header,table],className="panel panel-default")
 
 
-    return dbc.Container([panel])
+    return dbc.Container([title,description_block])
 
 #Page Generator
 def gen_page(run_id, test_id):
     run_summary=db.retrieve_test(run_id,test_id)
     base=yaml_input.load_yaml_from_run(run_id)
+    r=db.load_report(run_id)
+    report=val.Report(base,r)
+    return dbc.Container(metric_table(report.success[test_id]))
+    #return dbc.Container(str((run_summary,base["Tests"][test_id])))
 
     data={}
     data["description"]="Fake description"
     data["result"]="Success!"
     data["time"]="666"
+    data["test_id"]="<placeholder_testname>"
+    data["status"]="missing_validation"
     return summary_panel(data)
 
 
