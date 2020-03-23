aef86a3718b3ad5195811f01ce8d633d5624042e
jmcc <jean-marie.couteyen-carpaye@airbus.com>
2020-03-23 18:47:49 +0100

    jobrunner : reflect changes in db

--- gcvb/jobrunner.py
+++ gcvb/jobrunner.py
@@ -12,23 +12,25 @@ from . import yaml_input
 exit_success = 0
 
 class Job(object):
-    def __init__(self, run_id, test_id, test_id_db, launch_command, num_process, num_threads, job_type):
+    def __init__(self, run_id, test_id, test_id_db, step, launch_command, num_process, num_threads, job_type):
         self.run_id = run_id
         self.test_id = test_id
         self.test_id_db = test_id_db
+        self.step = step
         self.launch_command = launch_command
         self.num_process = int(num_process)
         self.num_threads = int(num_threads)
         self.type = job_type
         self.is_first = False
         self.is_last = False
+        self.return_code = -1
     def num_cores(self):
         return self.num_process*self.num_threads
     def run(self):
         env = dict(os.environ)
         env["GCVB_RUN_ID"] = f"{self.run_id}"
         env["GCVB_TEST_ID"] = f"{self.test_id_db}"
-        return subprocess.call(self.launch_command, shell=True, cwd=self.test_id, env=env)
+        self.return_code = subprocess.call(self.launch_command, shell=True, cwd=self.test_id, env=env)
     def name(self):
         return f"{self.test_id}_{self.num_process}x{self.num_threads}_{self.type}"
     def __repr__(self):
@@ -54,33 +56,34 @@ class JobRunner(object):
         tests_for_current_run = db.get_tests(self.run_id)
 
         data_root = tmp["data_root"]
-        self.tests = {t["name"] : [] for t in tests_for_current_run}
+        self.tests = {t["name"] : {} for t in tests_for_current_run}
         test_id_in_db = {t["name"] : t["id"] for t in tests_for_current_run}
         test_list = list(self.tests.keys())
         ref_valid = yaml_input.get_references([test_informations[n] for n in test_list],data_root)
-        for test,l in self.tests.items():
+        for test,tasks in self.tests.items():
             current_test = test_informations[test]
+            step = 0
             for c, task in enumerate(current_test["Tasks"]):
+                step += 1
                 at_job_creation = {}
                 gcvb_job.fill_at_job_creation_task(at_job_creation, task, f"{current_test['id']}_{c}", self.config)
                 launch_command = gcvb_job.format_launch_command(task["launch_command"],
                                                                 self.config, at_job_creation)
-                j=Job(self.run_id, test, test_id_in_db[test], launch_command,
+                j=Job(self.run_id, test, test_id_in_db[test], step, launch_command,
                           at_job_creation["nprocs"], at_job_creation["nthreads"], "task")
-                l.append(j)
+                tasks[step] = j
                 for d, val in enumerate(task.get("Validations",[])):
+                    step += 1
                     gcvb_job.fill_at_job_creation_validation(at_job_creation, val,
                                                              data_root, current_test["data"], config, ref_valid)
                     launch_command = gcvb_job.format_launch_command(val["launch_command"],
                                                                     self.config, at_job_creation)
-                    j = Job(self.run_id, test, test_id_in_db[test], launch_command,
+                    j = Job(self.run_id, test, test_id_in_db[test], step, launch_command,
                             at_job_creation["nprocs"], at_job_creation["nthreads"], "validation")
-                    l.append(j)
-            l[0].is_first = True
-            l[-1].is_last = True
+                    tasks[step] = j
+            tasks[1].is_first = True
+            tasks[step].is_last = True
 
-        self.started_locally = set()
-        self.failed_tests = set() # set of tests for which at least one task returned something else than sys.EX_OK
 
         # Go to the right directory
         os.chdir(computation_dir)
@@ -88,20 +91,30 @@ class JobRunner(object):
         db.set_db(os.path.abspath("../../gcvb.db"))
 
     def _run_job(self, job):
-        return_code = job.run()
+        job.run()
         with self.lock:
             self.available_cores += job.num_cores()
             del self.running_tests[job.test_id]
-            if job.is_last or return_code!=exit_success:
-                conn = db.get_exclusive_access()
-                with conn:
-                    cursor = conn.cursor()
-                    req = """UPDATE test SET end_date = CURRENT_TIMESTAMP
+
+            conn = db.get_exclusive_access()
+            with conn:
+                cursor = conn.cursor()
+                req = """UPDATE task
+                         SET end_date = CURRENT_TIMESTAMP, status = ?
+                         WHERE step = ? and test_id = ?"""
+                cursor.execute(req, [job.return_code, job.step, job.test_id_db])
+                if job.is_last or job.return_code!=exit_success:
+                    req = """UPDATE test
+                             SET end_date = CURRENT_TIMESTAMP, status = 2
                              WHERE id = ? and run_id = ?"""
                     cursor.execute(req,[job.test_id_db,job.run_id])
-                conn.close()
-            if return_code!=exit_success:
-                self.failed_tests.add(job.test_id)
+                else:
+                    req = """UPDATE test
+                             SET status = 0
+                             WHERE id = ? and run_id = ?"""
+                    cursor.execute(req,[job.test_id_db,job.run_id])
+            conn.close()
+
         with self.condition:
             self.condition.notify()
 
@@ -116,11 +129,9 @@ class JobRunner(object):
                     self.condition.wait()
             else:
                 with self.lock:
-                    self.tests[job.test_id].pop(0)
                     self.available_cores -= job.num_cores()
                     t = threading.Thread(name = job.name(), target = lambda: self._run_job(job))
                     self.running_tests[job.test_id] = t
-                    self.started_locally.add(job.test_id)
                 t.start()
             job = self._nextjob()
         db.end_run(self.run_id)
@@ -144,29 +155,36 @@ class JobRunner(object):
 
     def _nextjob(self):
         with self.lock:
-            # Queue of ready jobs
-            local_and_ready = [self.tests[test][0] for test in self.started_locally
-                                                   if self.tests[test] and
-                                                   test not in self.running_tests and
-                                                   test not in self.failed_tests]
-            if local_and_ready and self.local_first:
-                return self.elect_job(local_and_ready)
-
             # if we take elect an unstarted job, we must mark it as started while the database is locked for us.
             # the elect process must be done with the lock on the database.
             conn = db.get_exclusive_access()
             with conn:
                 cursor = conn.cursor()
-                req = """SELECT * FROM test
-                         WHERE run_id = ?
-                           AND start_date IS NULL"""
+                # Get first pending task (status -2) of a pending test (status 0)
+                req = """SELECT MIN(step) as step, test_id, name FROM task
+                         INNER JOIN test ON task.test_id=test.id
+                         INNER JOIN run ON run.id = test.run_id
+                         WHERE task.status = -2
+                         GROUP BY test_id
+                         HAVING test.status = 0 AND run.id = ?"""
                 cursor.execute(req,[self.run_id])
-                available_from_db = [self.tests[test["name"]][0] for test in cursor.fetchall() if self.tests[test["name"]]]
-                available_jobs = local_and_ready+available_from_db
+                available_jobs = [self.tests[test["name"]][test["step"]] for test in cursor.fetchall()]
+                if (self.local_first):
+                  available_started_jobs = [j for j in available_jobs if j.step!=1]
+                  available_jobs = available_started_jobs if available_started_jobs else available_jobs
                 to_be_run = self.elect_job(available_jobs)
                 if to_be_run and to_be_run.is_first: #was elected from database
                     req = """UPDATE test SET start_date = CURRENT_TIMESTAMP
                              WHERE id = ? and run_id = ?"""
                     cursor.execute(req,[to_be_run.test_id_db,to_be_run.run_id])
+                if to_be_run:
+                    req = """UPDATE task
+                             SET start_date = CURRENT_TIMESTAMP, status = -1
+                             WHERE step = ? AND test_id = ?"""
+                    cursor.execute(req,[to_be_run.step, to_be_run.test_id_db])
+                    req = """UPDATE test
+                             SET status = 1
+                             WHERE id = ?"""
+                    cursor.execute(req,[to_be_run.test_id_db])
             conn.close()
             return to_be_run
