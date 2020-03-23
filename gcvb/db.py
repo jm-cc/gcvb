419adb5049e5dd6fc0cb77dfac83b1bb2fde7a5b
jmcc <jean-marie.couteyen-carpaye@airbus.com>
2020-03-23 13:26:17 +0100

    db : preparation for keeping information at a task level

--- gcvb/db.py
+++ gcvb/db.py
@@ -24,13 +24,24 @@ CREATE TABLE test(id         INTEGER PRIMARY KEY,
                   start_date INTEGER,
                   end_date   INTEGER,
                   run_id     INTEGER,
+                  status     INTEGER DEFAULT 0, -- 0 pending, 1 running, 2 completed
                   FOREIGN KEY(run_id) REFERENCES run(id));
 
-CREATE TABLE valid(id      INTEGER PRIMARY KEY,
-                   metric  TEXT,
-                   value   REAL,
-                   test_id INTEGER,
-                   FOREIGN KEY(test_id) REFERENCES test(id));
+CREATE TABLE task(id         INTEGER PRIMARY KEY,
+                  step       INTEGER,
+                  start_date INTEGER,
+                  end_date   INTEGER,
+                  test_id    INTEGER,
+                  status     INTEGER DEFAULT -2, -- >=0, exit_status | -1 running | -2 pending
+                  FOREIGN KEY(test_id) REFERENCES test(id));
+
+CREATE TABLE valid(id        INTEGER PRIMARY KEY,
+                   metric    TEXT,
+                   value     REAL,
+                   test_id   INTEGER,
+                   task_step INTEGER,
+                   FOREIGN KEY(task_step) REFERENCES task(step),
+                   FOREIGN KEY(test_id)   REFERENCES test(id));
 
 CREATE TABLE files(id       INTEGER PRIMARY KEY,
                    filename TEXT,
@@ -110,13 +121,13 @@ def add_tests(cursor, run, test_list):
 @with_connection
 def start_test(cursor,run,test_id):
     cursor.execute("""UPDATE test
-                      SET start_date = CURRENT_TIMESTAMP
+                      SET start_date = CURRENT_TIMESTAMP, status = 1
                       WHERE id = ? AND run_id = ?""",[test_id,run])
 
 @with_connection
 def end_test(cursor, run, test_id):
     cursor.execute("""UPDATE test
-                      SET end_date = CURRENT_TIMESTAMP
+                      SET end_date = CURRENT_TIMESTAMP, status = 2
                       WHERE id = ? AND run_id = ?""",[test_id,run])
 
 @with_connection
