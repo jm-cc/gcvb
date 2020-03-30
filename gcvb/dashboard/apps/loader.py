f26f500130d72eff670b1847c0f3346d60e81888
jmcc <jean-marie.couteyen-carpaye@airbus.com>
2020-03-30 10:54:56 +0200

    dashboard loader : also load references files associated with a gcvb base

--- gcvb/dashboard/apps/loader.py
+++ gcvb/dashboard/apps/loader.py
@@ -4,10 +4,13 @@ import gcvb.db as db
 class BaseLoader(object):
     def __init__(self):
         self.loaded = {}
+        self.references = {}
     def load_base(self, run_id):
         ya,mod = db.retrieve_input(run_id)
         if (ya,mod) not in self.loaded:
             self.loaded[(ya,mod)] = yaml_input.load_yaml(ya,mod)
+            refs = yaml_input.get_references(self.loaded[(ya,mod)]["Tests"].values(),"./data")
+            self.references.update(refs)
         return self.loaded[(ya,mod)]
 
 loader = BaseLoader()
