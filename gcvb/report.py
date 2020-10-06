1564254e506a7d5a703cc67cd6c941086a167d4b
Jerome Robert <jeromerobert@gmx.com>
2020-10-06 18:46:32 +0200

    List only failed test in report --html

--- gcvb/report.py
+++ gcvb/report.py
@@ -2,25 +2,36 @@ from . import model
 import xml.etree.ElementTree as ET
 from pprint import pformat
 
+_HTML_TEMPLATE="""
+<h1>GCVB results</h1>
+<ul>
+<li>Status: {status}</li>
+<li>Total number of tests: {num_tests}</li>
+<li>Number of completed tests: {num_compl}</li>
+<li>Number of failure: {num_failure}</li>
+<li>Start date: {start}</li>
+<li>End date: {end}</li>
+</ul>
+<h2>Failed tests</h2>
+<ul>"""
 
 def html_report(run):
-    root = ET.Element("html")
-    body = ET.SubElement(root, "body")
-    title = ET.SubElement(body, "h1")
-    title.text = "GCVB Result"
-    sumary = ET.SubElement(body, "p")
-    sumary.text = run.str_status()
-    result_table = ET.SubElement(body, "table")
+    status = run.str_status()
+    num_tests = len(run.Tests)
+    num_compl = 0
+    failures = run.get_failures()
+    num_failure = len(failures)
+    start = run.start_date
+    end = run.end_date
+    strfailures = []
+    for testid in sorted(run.Tests.keys()):
+        t = run.Tests[testid]
+        if t.failed:
+            strfailures.append(f"<li>{testid}: {t.hr_result()}</li>")
+        if t.completed:
+            num_compl += 1
+    return "\n".join([_HTML_TEMPLATE.format(**locals())]+strfailures+["</ul>"])
 
-    for test_id, test in run.Tests.items():
-        line = ET.Element("tr")
-        test_str = ET.SubElement(line, "td")
-        test_str.text = test_id
-        result = ET.SubElement(line, "td")
-        result.text = test.hr_result()
-        result_table.append(line)
-
-    return ET.tostring(root, encoding="unicode")
 
 def str_report(run):
     rl = len(run.get_running_tests())
