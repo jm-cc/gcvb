45bf888f0ac6a31a1054c91626564d846edbb1d3
Jerome Robert <jeromerobert@gmx.com>
2020-11-04 13:54:23 +0100

    Remove dirty logging

--- gcvb/dashboard/assets/sortable.js
+++ gcvb/dashboard/assets/sortable.js
@@ -5,8 +5,6 @@
         v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
     )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-    document.querySelectorAll('th').forEach(th => console.log(th));
-
     document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
         const table = th.closest('table').querySelector('tbody');
         Array.from(table.querySelectorAll('tr'))
